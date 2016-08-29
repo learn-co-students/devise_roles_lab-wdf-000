8d7c686e5c63636598949e8abc08078e835e7b9d
Raycent <raycent.tan@flatironschool.com>
2016-08-29 18:20:49 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def update
+    Post.find(params[:id]).update(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
