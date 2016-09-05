ff65becf91b66667265448162af2b33f4510bf8f
aehmt <calisahmet@gmail.com>
2016-09-05 19:31:36 -0400

    2 tests failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def index
 
-    # @users = User.all
+    @users = User.all
     #
     # redirect_to users_path
   end
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    # binding.pry
     @user = User.find_by(id: params[:id])
     return head(:forbidden) unless current_user.try(:id) == @user.id || current_user.admin?
     @user.destroy
