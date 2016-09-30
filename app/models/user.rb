bfffb7e3c22e82de8f658ff9b8e5f60d9e9b4f7c
fidelscodes <fidelscodes@gmail.com>
2016-09-30 11:35:56 -0400

    set default role as :user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+  # NOTE: Only admin can update/destroy users
   enum role: [:user, :vip, :admin]
+
+  # Handles setting default role to :user. Maybe there's a cleaner, simpler solution?
+  after_initialize :set_default_role
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
