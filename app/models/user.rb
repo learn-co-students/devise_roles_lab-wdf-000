9362cb6a58ed191d0bcf8ec44d4656e64b942513
Natalisp <nathalipat@gmail.com>
2016-10-02 19:42:05 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
+  after_initialize :user_default
+
+
+  def user_default
+    self.role = "user"
+  end
+
 end
