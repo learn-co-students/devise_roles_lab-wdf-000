0e70c06acdb80a466d191b654ec76aa98d5bb05d
ozPop <ozziepopovas@gmail.com>
2016-10-03 17:45:52 -0400

    update posts_controller, add update view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :destroy]
   def index
   end
 
@@ -15,8 +16,25 @@ class PostsController < ApplicationController
   end
 
   def update
+    return access_denied(posts_path) unless current_user.admin? || current_user.vip?
+    @post.update(post_params)
+    redirect_to posts_path
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def access_denied(path)
+    redirect_to path, alert: 'Access denied.'
   end
 end
