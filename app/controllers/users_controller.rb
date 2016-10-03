74089b61b936a56cb1263afbefe3cb4d3e832968
ozPop <ozziepopovas@gmail.com>
2016-10-03 16:22:35 -0400

    update user_controller, index, add show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
   def new
+  end
+
+  def show
+    redirect_to root_path, alert: 'Access denied.' unless current_user == @user
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
