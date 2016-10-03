f5e7e932aa11e342b397ce81ad4ae397790c0bf1
ozPop <ozziepopovas@gmail.com>
2016-10-03 17:22:13 -0400

    generate post model, migration, add associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118210027) do
+ActiveRecord::Schema.define(version: 20161003211848) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
