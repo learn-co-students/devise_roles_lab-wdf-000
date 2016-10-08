8b381249a4e985e5669327363f80329a920b512b
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-08 02:49:44 -0400

    added some views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,40 @@
 class PostsController < ApplicationController
   
   before_action :admin_or_vip_role, only: [:update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :set_post, only: [:new, :edit]
+
+
+  def new
+
+  end
+
+  def create
+    post = Post.create(post_params)
+    redirect_to post_path(post)
+  end
 
   def update
     post = Post.find_by(params[:id])
     post.update(post_params)
-    redirect_to root_path
+    redirect_to post_path(post)
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to root_path
+    redirect_to post_path(post)
+  end
 
+  def show
+    @post = Post.find_by_id(params[:id])
   end
 
   private
+  
+  def set_post
+    @post = Post.find_by_id(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content, :user_id)
