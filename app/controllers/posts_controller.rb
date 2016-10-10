627b3d61b21470516660c2731deeee483b260061
Wendy Xu <guangw.xu@gmail.com>
2016-10-10 17:37:20 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,49 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_post, only: [:edit, :show, :update]
+
+
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    user_id = current_user.id
+    @post = Post.new(post_params)
+    @post.user_id = user_id
+    @post.save
+    redirect_to user_path(@post.user)
+  end
+  
   def show
-    @post = Post.find_by(id: params[:id])
+
+  end
+
+  def edit
+    unless current_user.admin? || current_user.vip? || current_user.try(:id) == @post.user_id
+      flash[:alert] = "Access denied."
+      redirect_to post_path(@post)
+    end
   end
 
   def update
-    @post = Post.find_by(id: params[:id])
-    @post.update(post_params) if current_user.admin? || current_user.vip? || current_user.try(:id) == @post.user_id
-    flash[:error] = "Access denied."
+
+    if current_user.admin? || current_user.vip? || current_user.try(:id) == @post.user_id
+      @post.update(post_params)
+      flash[:notice] = "Post updated!"
+    else
+      flash[:alert] = "Access denied."
+    end
     redirect_to post_path(@post) 
   end
 
 
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     unless @user == current_user
-      flash[:error] = "Access denied."
+      flash[:alert] = "Access denied."
       redirect_to root_path
     end
   end
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find_by(id: params[:id])
     @user.delete if current_user.admin?
-    flash[:error] = "Access denied."
+    flash[:alert] = "Access denied."
     redirect_to root_path
   end
 end
